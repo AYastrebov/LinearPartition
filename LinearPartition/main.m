2f3f23ec05f56f8caba0e52a6af89b2ceea84105
Andrey Yastrebov <andrey.yastrebov@corp.mail.ru>
2016-12-08 20:03:53 +0300

    Initial Commit

---
+++ LinearPartition/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinearPartition
+//
+//  Created by Andrey Yastrebov on 08.12.16.
+//  Copyright © 2016 Andrey Yastrebov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
