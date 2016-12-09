85553760887755374831fb6a4d866ea93a6b3eb1
Andrey Yastrebov <andrey.yastrebov@corp.mail.ru>
2016-12-09 11:16:20 +0300

    - initial commit

--- LinearPartition/main.m
+++ LinearPartition/main.m
@@ -8,10 +8,106 @@
 
 #import <Foundation/Foundation.h>
 
+NSArray *linearPartition(NSArray<NSNumber *> *seq, NSInteger k);
+NSArray<NSArray<NSNumber *> *> *buildPartitionTable(NSArray<NSNumber *> *seq, NSInteger k);
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        NSArray *input = @[@100, @200, @300, @400, @500, @600, @700, @800, @900];
+
+        NSArray *linear = linearPartition(input, 3);
+
+        NSLog(@"Result: %@", linear);
+
     }
     return 0;
 }
+
+NSArray *linearPartition(NSArray<NSNumber *> *seq, NSInteger k) {
+
+    NSMutableArray *result = [NSMutableArray array];
+
+    if (k <= 0) {
+        NSMutableArray<NSNumber *> *partition = [NSMutableArray array];
+        [partition addObjectsFromArray:seq];
+        [result addObject:partition];
+        return result;
+    }
+
+    NSUInteger n = seq.count - 1;
+
+    if (k > n) {
+        for (NSNumber *value in seq) {
+            NSMutableArray<NSNumber *> *partition = [NSMutableArray array];
+            [partition addObject:value];
+            [result addObject:partition];
+        }
+        return result;
+    }
+
+    NSArray<NSArray<NSNumber *> *> *table = buildPartitionTable(seq, k);
+
+    k = k-2;
+
+    while (k >= 0) {
+        NSMutableArray<NSNumber *> *partition = [NSMutableArray array];
+
+        for (NSInteger i = table[n-1][k].integerValue + 1; i < n + 1; i++) {
+            [partition addObject:seq[i]];
+        }
+
+        [result insertObject:partition atIndex:0];
+        n = table[n-1][k].integerValue;
+        k = k-1;
+    }
+
+    NSMutableArray<NSNumber *> *partition = [NSMutableArray array];
+
+    for (NSInteger i = 0; i < n+1; i++) {
+        [partition addObject:seq[i]];
+    }
+
+    [result insertObject:partition atIndex:0];
+
+    return result.copy;
+}
+
+NSArray<NSArray<NSNumber *> *> *buildPartitionTable(NSArray<NSNumber *> *seq, NSInteger k) {
+
+    NSUInteger n = seq.count;
+
+    NSMutableArray<NSMutableArray<NSNumber *> *> *table = [NSMutableArray arrayWithCapacity:n];
+    for (NSInteger i = 0; i < n; i++) {
+        [table addObject:[NSMutableArray arrayWithCapacity:k]];
+    }
+
+    NSMutableArray<NSMutableArray<NSNumber *> *> *solution = [NSMutableArray arrayWithCapacity:n - 1];
+    for (NSInteger i = 0; i < n - 1; i++) {
+        [solution addObject:[NSMutableArray arrayWithCapacity:k - 1]];
+    }
+
+    for (NSInteger i = 0; i < n; i++) {
+        table[i][0] = @(seq[i].integerValue + ((i > 0) ? (table[i-1][0].integerValue) : 0));
+    }
+
+    for (NSInteger j = 0; j < k; j++) {
+        table[0][j] = seq[0];
+    }
+
+    for (NSInteger i = 1; i < n; i++) {
+        for (NSInteger j = 1; j < k; j++) {
+            table[i][j] = @NSUIntegerMax;
+            for (NSInteger x = 0; x < i; x++) {
+                float cost = MAX(table[x][j-1].floatValue, table[i][0].floatValue - table[x][0].floatValue);
+                if (table[i][j].floatValue > cost) {
+                    table[i][j] = @(cost);
+                    solution[i-1][j-1] = @(x);
+                }
+            }
+        }
+    }
+
+    return solution.copy;
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Linear partition
+# Partitions a sequence of non-negative integers into k ranges
+# Also see http://www8.cs.umu.se/kurser/TDBAfl/VT06/algorithms/BOOK/BOOK2/NODE45.HTM
